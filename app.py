bc6f0191426caff11091fab927e1d4987228d33d
Richard Guaman <gs.richarda@gmail.com>
2021-04-16 13:04:08 +0100

    small bug fixed

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ fs = gridfs.GridFS(mongo.db)  # create GridFS instance
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=20)
+    app.permanent_session_lifetime = timedelta(minutes=10)
 
 
 # # -----> OUTSIDE DASHBOARD PAGES & FUNCTIONS <-----
